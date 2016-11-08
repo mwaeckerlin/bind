2b0059235adde7438f18b1259b70b91aa1f0cadb
Marc Wäckerlin <marc@tech.swisssign.net>
2016-11-08 14:02:11 +0100

    first release

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM ubuntu
+MAINTAINER mwaeckerlin
+ENV TERM xterm
+
+EXPOSE 53
+
+ENV TTL "3600"
+ENV SERIAL ""
+ENV REFRESH "3600"
+ENV RETRY "1800"
+ENV EXPIRE "604800"
+ENV NEGATIVE_CACHE_TTL "1800"
+ENV DEFAULT_IP ""
+ENV DEFAULT_SUBDOMAINS "www mail"
+ENV DEAFULT_DOMAINS ""
+
+RUN apt-get update
+RUN apt-get install -y bind9
+
+ADD start.sh /start.sh
+CMD start.sh
---
+++ start.sh
@@ -0,0 +1,60 @@
+#! /bin/bash
+
+rm /etc/bind/named.conf.local
+
+IFS="
+"
+for line in $(env | egrep '^[-.0-9a-z]*=') $(echo ${DEFAULT_DOMAINS} | tr ' ' '\n'); do
+    echo "line=$line"
+    base=${line%%=*}
+    echo "base=$base"
+    [[ $line =~ = ]] && args=${line#*=} || args=''
+    echo "args=$args"
+    ip=${args%%;*}
+    echo "ip=$ip"
+    [[ $args =~ \; ]] && args=${args#*;} || args=''
+    echo "args=$args"
+    subs=${args%%;*}
+    echo "subs=$subs"
+    [[ $args =~ \; ]] && args=${args#*;} || args=''
+    echo "args=$args"
+    echo "... domain: $base on $ip with $subs and $args"
+    cat > "/etc/bind/$base" <<EOF
+\$TTL	${TTL}
+@	IN	SOA	${base}. root.${base}. (
+			${SERIAL:-$(date +%s)}	; Serial
+			${REFRESH}	; Refresh
+			${RETRY}	; Retry
+			${EXPIRE}	; Expire
+			${NEGATIVE_CACHE_TTL} )	; Negative Cache TTL
+;
+@	IN	NS	@
+@	IN	A	${ip:-${DEFAULT_IP}}
+@	IN	MX 10	@
+EOF
+    IFS=" "
+    for sub in ${subs:-${DEFAULT_SUBDOMAINS}}; do
+        [[ $sub =~ = ]] && addr=${sub#*=} || addr=''
+        [[ $addr =~ : ]] && t=${addr%%:*} || t=''
+        addr=${addr#*:}
+        cat >> "/etc/bind/$base" <<EOF
+${sub%%=*}	IN	${t:-CNAME}	${addr:-@}
+EOF
+    done
+    IFS=';'
+    for rec in $args; do
+        cat >> "/etc/bind/$base" <<EOF
+$rec
+EOF
+    done
+    cat >> /etc/bind/named.conf.local <<EOF
+zone "${base}" {
+	type master;
+	allow-query { any; };
+	file "/etc/bind/$base";
+};
+EOF
+done
+
+echo "ready."
+named -f
